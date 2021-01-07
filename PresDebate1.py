ebc8494cd4537b644f9860711cda400c5c7e74ce
affyzal <32396442+affyzal@users.noreply.github.com>
2021-01-07 15:23:40 +0000

    added read file function

--- PresDebate1.py
+++ PresDebate1.py
@@ -1,5 +1,40 @@
 import csv
 import numpy as np
 import pandas as pd
-
+import os
 import spacy
+import matplotlib
+import nltk
+import string
+import plotly
+import warnings
+import datetime
+import wordcloud
+
+
+def main():
+    print("Helloworld")
+    debate1 = readfile("us_election_2020_1st_presidential_debate.csv")
+
+
+def readfile(filetoread):
+    file = filetoread
+    df = pd.read_csv(file)
+    print(df)
+    return df
+    # readfile
+    # return readin file
+
+
+def fixnull():
+    # find null values in csv
+    # fix
+    return
+
+
+def tidy():
+    # tidy up unnecessary words
+    return
+
+
+main()
