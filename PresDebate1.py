a76e3c90397e9a849d7ccdffb651572396800841
affyzal <32396442+affyzal@users.noreply.github.com>
2021-01-07 17:26:27 +0000

    added code to isolate by speaker

--- PresDebate1.py
+++ PresDebate1.py
@@ -11,22 +11,29 @@ import warnings
 import datetime
 import wordcloud
 
-
 def main():
     print("Helloworld")
     debate1 = readfile("us_election_2020_1st_presidential_debate.csv")
+    speaker = "Chris Wallace"
+    dfcw = isolatespeaker(speaker)
+    print(dfcw)
 
 
 def readfile(filetoread):
     file = filetoread
     df = pd.read_csv(file)
-    print(df)
+    #print(df[100:107])
     return df
+
     # readfile
     # return readin file
 
-def isolatespeaker(speaker):
-    return
+
+def isolatespeaker(isolate):
+    df = pd.read_csv("us_election_2020_1st_presidential_debate.csv")
+    dfcw = df[df['speaker'] == 'Chris Wallace']
+    return dfcw
+
 
 def fixnull():
     # find null values in csv
