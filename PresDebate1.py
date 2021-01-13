a33edce8292ec865f9a59d46603f645f7faf5310
affyzal <32396442+affyzal@users.noreply.github.com>
2021-01-13 14:47:24 +0000

    started to fix dataframe to have one part from 0:00 to end, not multiple parts with multiple 0:00

--- PresDebate1.py
+++ PresDebate1.py
@@ -1,6 +1,7 @@
 import csv
 import numpy as np
 import pandas as pd
+import math
 import os
 import spacy
 import matplotlib
@@ -11,28 +12,48 @@ import warnings
 import datetime
 import wordcloud
 
+#TODO : FIX NAN VALUE IN TIME IN DEBATE AND MAKE DATAFRAME INTO ONE PART
+#TODO : FIX TIME VALUES IN DEABATE TO NOT BE SPLIT UP INTO PARTS AND MAKE DATAFRAME INTO ONE PART
 
 def main():
     debate1 = readfile("us_election_2020_1st_presidential_debate.csv")
-    dfcw, dfdt, dfjb = isolatespeaker(debate1)
-    dftext = isolatetext(dfcw)
+    dfcw, dfdt, dfjb = isolatespeaker(debate1, 1)
+    cwtext = isolatetext(dfcw)
+    dttext = isolatetext(dfdt)
+    jbtext = isolatetext(dfjb)
+
+    debate2 = readfile("us_election_2020_2nd_presidential_debate.csv")
+    dfkw, dfdt2, dfjb2 = isolatespeaker(debate2, 2)
+    cwtext2 = isolatetext(dfkw)
+    dttext2 = isolatetext(dfdt2)
+    jbtext2 = isolatetext(dfjb2)
+    print(debate1.speaker.unique())
+    print(debate2.speaker.unique())
+
 
 def readfile(filetoread):
     file = filetoread
     df = pd.read_csv(file)
     return df
 
-
 #TODO : Add logic to work for every debate
-def isolatespeaker(debatefile):
+def isolatespeaker(debatefile, debatenum):
     df = debatefile
-    dfcw = df[df['speaker'] == 'Chris Wallace']
-    dfdt = df[df['speaker'] == 'President Donald J. Trump']
-    dfjb = df[df['speaker'] == 'Vice President Joe Biden']
-    return dfcw, dfdt, dfjb
+    if debatenum == 1:
+        host = df[df['speaker'] == 'Chris Wallace']
+        dfdt = df[df['speaker'] == 'President Donald J. Trump']
+        dfjb = df[df['speaker'] == 'Vice President Joe Biden']
+    else:
+        host = df[df['speaker'] == 'Kristen Welker']
+        dfdt = df[df['speaker'] == 'Donald Trump']
+        dfjb = df[df['speaker'] == 'Joe Biden']
+
+    #dfdt = df[df['speaker'] == 'President Donald J. Trump']
+    #dfjb = df[df['speaker'] == 'Vice President Joe Biden']
+    return host, dfdt, dfjb
 
 
-def fixnull():
+def fixtimeframe():
     # find null values in csv
     # fix
     return
@@ -46,7 +67,6 @@ def tidyjunk():
 def isolatetext(speakerfile):
     df = speakerfile
     df2 = df['text']
-    print(df2)
     return df2
 
 
