8200967f0b1684a3fceb7110c3a2a4e581bcd3a3
affyzal <32396442+affyzal@users.noreply.github.com>
2021-02-03 14:40:59 +0000

    started to fix dataframe to have one part from 0:00 to end, not multiple parts with multiple 0:00

--- PresDebate1.py
+++ PresDebate1.py
@@ -1,4 +1,6 @@
 import csv
+
+import inline as inline
 import numpy as np
 import pandas as pd
 import math
@@ -11,24 +13,48 @@ import plotly
 import warnings
 import datetime
 import wordcloud
+from textblob import TextBlob
+from PIL import Image
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import plotly.express as px
+import plotly.figure_factory as ff
+import warnings
+import os
+#%matplotlib inline
 
 #TODO : FIX NAN VALUE IN TIME IN DEBATE AND MAKE DATAFRAME INTO ONE PART
 #TODO : FIX TIME VALUES IN DEABATE TO NOT BE SPLIT UP INTO PARTS AND MAKE DATAFRAME INTO ONE PART
+from wordcloud import WordCloud
+
 
 def main():
     debate1 = readfile("us_election_2020_1st_presidential_debate.csv")
+    #FIX TIMEFRAME HERE
     dfcw, dfdt, dfjb = isolatespeaker(debate1, 1)
     cwtext = isolatetext(dfcw)
     dttext = isolatetext(dfdt)
     jbtext = isolatetext(dfjb)
 
     debate2 = readfile("us_election_2020_2nd_presidential_debate.csv")
+    #FIX TIMEFRAME HERE
     dfkw, dfdt2, dfjb2 = isolatespeaker(debate2, 2)
     cwtext2 = isolatetext(dfkw)
     dttext2 = isolatetext(dfdt2)
     jbtext2 = isolatetext(dfjb2)
+    print(cwtext)
+    print(dttext)
+    print(jbtext)
+
+    print(cwtext2)
+    print(dttext2)
+    print(jbtext2)
     print(debate1.speaker.unique())
     print(debate2.speaker.unique())
+
 
 
 def readfile(filetoread):
